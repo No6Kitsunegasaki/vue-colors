089f3c63b560d2e3435f26d3f6aaca5fdd2c3ebe
noroku kitsunegasaki <none.gingerale@gmail.com>
2018-04-19 20:28:35 +0900

    Initial commit

---
+++ src/components/Board.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="board" :style=style>
+    <panel v-for="(panel, key) in panels" :panel="panel" :key="key"/>
+  </div>
+</template>
+
+<script>
+import Panel from "./Panel";
+export default {
+  components: {
+    Panel
+  },
+  name: "Board",
+  props: ["rows", "columns"],
+  data() {
+    return {
+      panels: [],
+      style: { width: 0, height: 0 },
+      panelStyle: { width: 100, height: 100 }
+    };
+  },
+  created: function() {
+    const panelCount = this.rows * this.columns;
+    this.style.width = this.columns * this.panelStyle.width + "px";
+    this.style.height = this.rows * this.panelStyle.height + "px";
+    for (let i = 0; i < panelCount; i++) {
+      this.panels.push({ color: 0, style: this.panelStyle });
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.board {
+  border: 1px solid #000000;
+  display: flex;
+  flex-wrap: wrap;
+  margin: auto;
+  padding: auto;
+}
+</style>
---
+++ src/components/Panel.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="panel" :class="classObject" :style="style" v-on:click="change" >
+    <div>
+      bgcolor: {{bgcolor}}
+      color index: {{panel.color}}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Panel",
+  data() {
+    return {
+      list: [
+        "color-red",
+        "color-orange",
+        "color-yellow",
+        "color-green",
+        "color-blue",
+        "color-navy",
+        "color-purple"
+      ]
+    };
+  },
+  props: ["panel"],
+  methods: {
+    change: function(event) {
+      this.panel.color++;
+      if (this.panel.color >= this.list.length) {
+        this.panel.color = 0;
+      }
+    }
+  },
+  computed: {
+    classObject: function() {
+      let obj = {};
+      obj[this.list[this.panel.color]] = true;
+      return obj;
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.panel {
+  height: 100px;
+  width: 100px;
+  flex-basis: 100px;
+}
+.color-red {
+  background-color: #c93a40;
+}
+.color-orange {
+  background-color: orange;
+}
+.color-yellow {
+  background-color: yellow;
+}
+.color-green {
+  background-color: green;
+}
+.color-blue {
+  background-color: blue;
+}
+.color-navy {
+  background-color: navy;
+}
+.color-purple {
+  background-color: purple;
+}
+</style>
